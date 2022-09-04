313a1d713250c56b2867b27d317c4ce18c858b83
genivanss <genivanss@yahoo.com.br>
2022-09-03 23:54:36 -0300

    Commit da etapa 4

---
+++ src/main/java/io/github/genivanss/MinhaConfiguration.java
@@ -0,0 +1,14 @@
+package io.github.genivanss;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinhaConfiguration {
+
+    @Bean(name = "applicationName")
+    public String applicationName(){
+        return "Sistema de Vendas";
+    }
+
+}
--- src/main/java/io/github/genivanss/VendasApplication.java
+++ src/main/java/io/github/genivanss/VendasApplication.java
@@ -1,5 +1,7 @@
 package io.github.genivanss;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class VendasApplication {
+    @Autowired
+    @Qualifier("applicationName")
+    private String applicationName;
     @GetMapping("/hello")
     public String helloWorld(){
-        return "hello world";
+        return applicationName;
     }
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class,args);
